81b9bcf15e780a0cc1813dec4ef455734f63142a
Gerrit Acheson <gerritacheson@Gerrits-MacBook.local>
2020-01-25 23:03:49 -0500

    fixing my java

---
+++ js/main.js
@@ -0,0 +1,19 @@
+(() => {
+  console.log("Hello from JS!");
+
+// find the elements in the document
+let allSVGS = document.querySelectorAll(".svg");
+
+// this function should change the header log
+function changeText() {
+  toggleButton.textContent = "What is up? I am script!";
+}
+
+// this funtion should log the ID for the badge
+function logSVG() {
+  console.log(this.id);
+}
+
+allSVGS.forEach(item => item.addEventListener("click", logSVG));
+
+})();
